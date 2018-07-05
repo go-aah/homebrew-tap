5510c2016fc9d77c04f0bbe8513e8b4be65946c1
Jeevanandam M <jeeva@myjeeva.com>
2018-07-05 01:55:28 -0700

    brew tap update with 0.11.0-edge release

--- Formula/aah.rb
+++ Formula/aah.rb
@@ -2,7 +2,7 @@ class Aah < Formula
     desc "aah framework CLI, a developer assistant"
     homepage "https://aahframework.org"
     url "https://dl.aahframework.org/releases/cli/0.11.0-edge/aah_darwin_amd64.zip"
-    sha256 "e834649c60abe060e3ce82f1831e1c7fe156bcd61d5709d877af5aca3f944edf"
+    sha256 "addee299692f112500d68b7c179f15cb7895a76cd824670d8052295c6200f41c"
     version "0.11.0-edge"
 
     def install
