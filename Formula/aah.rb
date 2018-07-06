d67395cb072a7b1a49aaa3574cc1a3edc4cbd31e
Jeevanandam M <jeeva@myjeeva.com>
2018-07-05 21:50:27 -0700

    further improvements and linux block added

--- Formula/aah.rb
+++ Formula/aah.rb
@@ -1,15 +1,31 @@
 class Aah < Formula
     desc "aah framework CLI, a developer assistant"
     homepage "https://aahframework.org"
-    url "https://dl.aahframework.org/releases/cli/0.11.0-edge/aah_darwin_amd64.zip"
-    sha256 "addee299692f112500d68b7c179f15cb7895a76cd824670d8052295c6200f41c"
     version "0.11.0-edge"
+
+    depends_on "go"
+
+    if OS.mac?
+        url "https://dl.aahframework.org/releases/cli/0.11.0-edge/aah_darwin_amd64.zip"
+        sha256 "addee299692f112500d68b7c179f15cb7895a76cd824670d8052295c6200f41c"
+    elsif OS.linux?
+        # upcoming :)
+    else
+        onoe "Unsupported operating system"
+    end
 
     def install
         bin.install "aah"
     end
 
+    def caveats; <<~EOS
+        You may wish to delete `$GOPATH/bin/aah` binary, Since you 
+        have successfully installed aah CLI binary distribution.
+    EOS
+    end
+
     test do
-        system "#{bin}/aah -v"
+        #system "#{bin}/aah -v"
+        assert_match "cli v#{version}", shell_output("#{bin}/aah -v")
     end
 end
