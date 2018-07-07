b77e8f75aed30f5c5ebe0ad731614dd216503788
Jeevanandam M <jeeva@myjeeva.com>
2018-07-06 22:41:49 -0700

    brew tap update with 0.11.0 release

--- Formula/aah.rb
+++ Formula/aah.rb
@@ -1,13 +1,11 @@
 class Aah < Formula
     desc "aah framework CLI, a developer assistant"
     homepage "https://aahframework.org"
-    version "0.11.0-edge"
-
-    depends_on "go"
+    version "0.11.0"
 
     if OS.mac?
-        url "https://dl.aahframework.org/releases/cli/0.11.0-edge/aah_darwin_amd64.zip"
-        sha256 "addee299692f112500d68b7c179f15cb7895a76cd824670d8052295c6200f41c"
+        url "https://dl.aahframework.org/releases/cli/0.11.0/aah_darwin_amd64.zip"
+        sha256 "4acbd43a00c48f6234b1c3a68e3a7b12d3a98eea4af9533c56637bf10bce0aaa"
     elsif OS.linux?
         # upcoming :)
     else
@@ -19,13 +17,12 @@ class Aah < Formula
     end
 
     def caveats; <<~EOS
-        You may wish to delete `$GOPATH/bin/aah` binary, Since you 
-        have successfully installed aah CLI binary distribution.
+        You may wish to delete `$GOPATH/bin/aah` binary to have smooth experience.
+        Since you have successfully installed aah CLI binary distribution.
     EOS
     end
 
     test do
-        #system "#{bin}/aah -v"
         assert_match "cli v#{version}", shell_output("#{bin}/aah -v")
     end
 end
